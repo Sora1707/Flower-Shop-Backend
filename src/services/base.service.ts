c4a0463ea8a02001afcf702421f74e3bd06899ab
Sora <volan1301@gmail.com>
2025-06-11 15:53:46 +0700

    [BASE SERVICE] Basic CRUD functions accomplished

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -25,18 +25,33 @@ export abstract class BaseService<T> {
         return item;
     }
 
-    public async findMany(filter: FilterQuery<T>) {
+    public async findMany(filter: FilterQuery<T>): Promise<T[]> {
         const items = await this.model.find(filter);
         return items;
     }
 
-    public async create(input: Partial<T>) {
+    public async create(input: Partial<T>): Promise<T> {
         const item = await this.model.create(input);
         return item;
     }
 
-    public async remove(filter: Partial<T> & { id: ObjectId }) {
-        const item = await this.model.deleteMany(filter);
+    public async deleteMany(filter: Partial<T> & { id: ObjectId }) {
+        const result = await this.model.deleteMany(filter);
+        return result;
+    }
+
+    public async deleteById(id: ObjectId) {
+        const item = await this.model.findByIdAndDelete(id);
+        return item;
+    }
+
+    public async updateMany(filter: Partial<T> & { id: ObjectId }, input: Partial<T>) {
+        const result = await this.model.updateMany(filter, input, { new: true });
+        return result;
+    }
+
+    public async updateById(id: ObjectId, input: Partial<T>) {
+        const item = await this.model.findByIdAndUpdate(id, input, { new: true });
         return item;
     }
 }
